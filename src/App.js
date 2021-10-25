8c89e1f936f52b772b8f50fee987b438bb7b5117
cavaglieridomenico <domenicomassimo.cavaglieri@gmail.com>
2021-10-25 18:00:31 +0200

    [ADDED] Creation of the three pages #2
    First setup of React Router.
    The pages in this phase can only be navigated from the address bar.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Contact from './pages/Contact';
+import Plan from './pages/Plan';
+import Preferences from './pages/Preferences';
 
 function App() {
   return (
-    <>
-      <h1>Hello!</h1>
-    </>
+    <Router>
+      <Switch>
+        <Route exact path='/'>
+          <Contact />
+        </Route>
+        <Route path='/plan'>
+          <Plan />
+        </Route>
+        <Route path='/preferences'>
+          <Preferences />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
