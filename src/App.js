02b1cf35d4424066731b77085ca856d940a95e4b
cavaglieridomenico <domenicomassimo.cavaglieri@gmail.com>
2021-10-26 06:13:28 +0200

    [ADDED] Error page
    The error page appears if a name that does not correspond to any page
    is manually entered in the url bar.
    Also added the Component Link that takes you back to the Contact page.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Contact from './pages/Contact';
 import Plan from './pages/Plan';
 import Preferences from './pages/Preferences';
+import Error from './pages/Error';
 
 function App() {
   return (
@@ -16,6 +17,9 @@ function App() {
         </Route>
         <Route path='/preferences'>
           <Preferences />
+        </Route>
+        <Route path='*'>
+          <Error />
         </Route>
       </Switch>
     </Router>
