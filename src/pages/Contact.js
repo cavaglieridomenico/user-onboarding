d2cca15c5c78abfd4948edd7c499dfe37974380b
cavaglieridomenico <domenicomassimo.cavaglieri@gmail.com>
2021-10-27 13:43:15 +0200

    [ADDED] Element in Contact.js
    Added and stylized the titles, paragraphs and footer.
    Modified the responsiveness classes.

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -6,16 +6,31 @@ import LinkToModal from '../components/LinkToModal';
 
 const Contact = () => {
   return (
-    <div className='page-container'>
+    <div className='outer-container'>
       <Sidebar />
-      <section>
+      <div className='inner-container'>
         <Header number={1} />
-        <article>
+        <section>
           <h1>Сontact details</h1>
+          <article>
+            <p>
+              Welcome to United Properties, we are glad to see you! Let’s get
+              started by letting us know a little bit about you
+            </p>
+          </article>
+          <form></form>
+          <article>
+            <h2>Privacy policy</h2>
+            <p>
+              We know you care about how your personal information is used and
+              shared, so we take your privacy seriously
+            </p>
+          </article>
+
           <LinkToModal text={'Expand privacy policy'} />
-        </article>
-        <Footer page={'/plan'} />
-      </section>
+        </section>
+        <Footer page={'/plan'} textLink='Back to the homepage' />
+      </div>
     </div>
   );
 };
