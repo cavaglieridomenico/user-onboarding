e1c1dd9a9f472d7a35b62472f1a551b62efdf004
cavaglieridomenico <domenicomassimo.cavaglieri@gmail.com>
2021-10-27 09:53:33 +0200

    [FIXED] Header style
    Eliminated a container no longer useful.

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,9 @@ import LinkToModal from './LinkToModal';
 const header = ({ number }) => {
   return (
     <header>
-      <div className='text-container'>
-        <p className='header__step'>step {number} of 3</p>
-        <div className='header__help-container'>
+      <p className='header__step'>step {number} of 3</p>
+      <div className='header__help-container'>
+        <p>Lost or have trouble?</p>
         <LinkToModal text={'Get help'} />
       </div>
     </header>
