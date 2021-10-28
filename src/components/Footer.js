a8dd6663258df1ae279a13e9ed00fbad952a8c17
cavaglieridomenico <domenicomassimo.cavaglieri@gmail.com>
2021-10-28 12:48:34 +0200

    [ADDED] Buttons in the Footer
    Added the buttons with the relative style, optimized only in desktop
    view.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import LinkToPage from './LlnkToPage';
-
+import arrow_right_W from '../assets/images/arrow_right_w.svg';
 const Footer = ({ textLink, page }) => {
   return (
     <footer>
       <LinkToPage textLink={textLink} page={page} />
+      <div className='btn-container'>
+        <button className='btn btn-primary50'>Skip for now</button>
+        <button className='btn btn-primary'>
+          Next step
+          <img src={arrow_right_W} alt='arrow-icon' />
+        </button>
+      </div>
     </footer>
   );
 };
