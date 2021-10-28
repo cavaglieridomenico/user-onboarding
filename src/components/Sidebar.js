943d059bf9b6ac49fdb35d2e9cd2b11e24031b19
cavaglieridomenico <domenicomassimo.cavaglieri@gmail.com>
2021-10-28 16:44:03 +0200

    [ADDED] Process block in Sidebar.
    Added the block of process in the Sidebar and prepared the activation
    classes in sidebar.css, but not yet inserted in the Component.

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,6 +5,37 @@ const Sidebar = () => {
   return (
     <aside>
       <Logo view='logo-desktop' />
+      <div className='progress-container '>
+        <div className='progress'>
+          <div className='square-text-container'>
+            <div className='outer-square'>
+              <div className='inner-square'></div>
+            </div>
+            <p>Contact details</p>
+          </div>
+          <div className='rectangle'></div>
+          <div className='rectangle'></div>
+        </div>
+        <div className='progress'>
+          <div className='square-text-container'>
+            <div className='outer-square'>
+              <div className='inner-square'></div>
+            </div>
+            <p>Investment plans</p>
+          </div>
+          <div className='rectangle'></div>
+          <div className='rectangle'></div>
+        </div>
+        <div className='progress'>
+          <div className='square-text-container'>
+            <div className='outer-square'>
+              <div className='inner-square'></div>
+            </div>
+            <p>Investment preferences</p>
+          </div>
+        </div>
+      </div>
+      <div className='quote-container'></div>
     </aside>
   );
 };
