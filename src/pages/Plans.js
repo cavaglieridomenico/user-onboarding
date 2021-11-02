bb9a0c9563bd6d66575bcfe0de7e9b7edd2d5bda
cavaglieridomenico <domenicomassimo.cavaglieri@gmail.com>
2021-11-02 09:45:56 +0100

    [ADDED] Structure of the Plans.js page
    Added the JSX structure, with classes and id.

--- src/pages/Plans.js
+++ src/pages/Plans.js
@@ -5,13 +5,46 @@ import Footer from '../components/Footer';
 
 const Plans = () => {
   return (
-    <div className='container'>
+    <div className='onboarding-outerbox'>
       <Sidebar />
-      <section>
-        <Header />
-        <h1>Plans</h1>
-        <Footer page={'/preferences'} />
-      </section>
+      <div className='onboarding-innerbox'>
+        <Header number={2} />
+        <section className='plans'>
+          <h1>Investiment Plans</h1>
+          <article>
+            <p>
+              Let us know about your investment plans. This will help us get you
+              to the right expert who will help you further
+            </p>
+          </article>
+          <article>
+            <h2>How much are you planning to invest in this year?</h2>
+            <form id='plans'>
+              <div className='form-container'>
+                <div className='from-box'>
+                  <label htmlFor='plans-from'>From</label>
+                  <input type='text' id='plans-from' />
+                </div>
+                <div className='to-box'>
+                  <label htmlFor='plans-to'>To</label>
+                  <input type='text' id='plans-to' />
+                </div>
+              </div>
+              <div className='slider'></div>
+            </form>
+            <article>
+              <h2>Are you an accredited investor?</h2>
+              <form action='' id='plans-investor'>
+                <input type='radio' name='accredited' value='yes' />
+                Yes
+                <input type='radio' name='accredited' value='no' />
+                No
+              </form>
+            </article>
+          </article>
+        </section>
+        <Footer nextPage={'/preferences'} homePage={'/'} />
+      </div>
     </div>
   );
 };
