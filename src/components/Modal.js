c0f6a3b590b007e26178172974815c6eb3d91f0d
cavaglieridomenico <domenicomassimo.cavaglieri@gmail.com>
2021-11-07 18:28:33 +0100

    [ADDED] Structure, logic and style of the Modal
    which appears as confirmation of the user's data registration.

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -12,15 +12,43 @@ const Modal = () => {
           <button className='modal-close' onClick={closeModal}>
             <GrClose />
           </button>
-          <h1>Registered user</h1>
+          <h1 className='modal-title'>Registered user</h1>
           <section>
             <p></p>
-            <div className='response-container'></div>
+            <div className='response-container'>
+              <ul>
+                <li>Identification number: </li>
+                <li>{response.id}</li>
+                <li>Full name: </li>
+                <li>{response.fullName}</li>
+                <li>Phone number:</li>
+                <li>
+                  {response.phoneCode} {response.phoneNumber}
+                </li>
+                <li>Email:</li>
+                <li>{response.email}</li>
+                <li>Country: </li>
+                <li>{response.country}</li>
+                <li>Investment plan from: </li>
+                <li>${response.planFrom}</li>
+                <li>Investment plan to: </li>
+                <li>${response.planTo}</li>
+                <li>Accredited investor: </li>
+                <li>{response.accredited}</li>
+                <li>Investment preferences: </li>
+                <li>
+                  {response.preferences.map((item, index) => {
+                    return <p key={index}>{item}</p>;
+                  })}
+                </li>
+              </ul>
+            </div>
           </section>
         </div>
       </div>
     );
   }
+  return <div></div>;
 };
 
 export default Modal;
