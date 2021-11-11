3af48c58db4e5f0efb593d39bfc4511f5fcd0891
cavaglieridomenico <domenicomassimo.cavaglieri@gmail.com>
2021-11-11 16:56:25 +0100

    [UPDATED] Validation progress in desktop view
    The Validation Progress has become a Component with its own style sheet.
    The sections in the Sidebar light up following the status of the pages
    actually validated. When the data is sent, a timeout begins to turn off
    the writings.

---
+++ src/components/Progress.js
@@ -0,0 +1,99 @@
+import React from 'react';
+import { useGlobalContext } from '../context';
+
+const Progress = () => {
+  const { stepStatus1, stepStatus2, stepStatus3 } = useGlobalContext();
+
+  return (
+    <div className='progress-container '>
+      <div className='progress'>
+        <div className='square-text-container'>
+          <div
+            className={`outer-square ${
+              stepStatus1 ? 'outer-active' : undefined
+            }`}
+          >
+            <div
+              className={`inner-square ${
+                stepStatus1 ? 'inner-active' : undefined
+              }`}
+            ></div>
+          </div>
+          <p
+            className={`progress-text ${
+              stepStatus1 ? 'progress-text-active' : undefined
+            }`}
+          >
+            Contact details
+          </p>
+        </div>
+        <div
+          className={`rectangle outer-square ${
+            stepStatus1 ? 'rectangle outer-active' : undefined
+          }`}
+        ></div>
+        <div
+          className={`rectangle outer-square ${
+            stepStatus1 ? 'rectangle outer-active' : undefined
+          }`}
+        ></div>
+      </div>
+      <div className='progress'>
+        <div className='square-text-container'>
+          <div
+            className={`outer-square ${
+              stepStatus2 ? 'outer-active' : undefined
+            }`}
+          >
+            <div
+              className={`inner-square ${
+                stepStatus2 ? 'inner-active' : undefined
+              }`}
+            ></div>
+          </div>
+          <p
+            className={`progress-text ${
+              stepStatus2 ? 'progress-text-active' : undefined
+            }`}
+          >
+            Investment plans
+          </p>
+        </div>
+        <div
+          className={`rectangle outer-square ${
+            stepStatus2 ? 'rectangle outer-active' : undefined
+          }`}
+        ></div>
+        <div
+          className={`rectangle outer-square ${
+            stepStatus2 ? 'rectangle outer-active' : undefined
+          }`}
+        ></div>
+      </div>
+      <div className='progress'>
+        <div className='square-text-container'>
+          <div
+            className={`outer-square ${
+              stepStatus3 ? 'outer-active' : undefined
+            }`}
+          >
+            <div
+              className={`inner-square ${
+                stepStatus3 ? 'inner-active' : undefined
+              }`}
+            ></div>
+          </div>
+          <p
+            className={`progress-text ${
+              stepStatus3 ? 'progress-text-active' : undefined
+            }`}
+          >
+            Investment preferences
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Progress;
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,68 +1,12 @@
 import React from 'react';
 import Logo from './Logo';
+import Progress from './Progress';
 
-const Sidebar = ({ page, text, author, role }) => {
+const Sidebar = ({ text, author, role }) => {
   return (
     <aside>
       <Logo view='logo-desktop' />
-      <div className='progress-container '>
-        <div className='progress'>
-          <div className='square-text-container'>
-            <div className='outer-square outer-active'>
-              <div className='inner-square inner-active'></div>
-            </div>
-            <p className='text-active'>Contact details</p>
-          </div>
-          <div className='rectangle outer-active'></div>
-          <div className='rectangle outer-active'></div>
-        </div>
-        <div className='progress'>
-          <div className='square-text-container'>
-            <div
-              className={`outer-square ${
-                page !== 'contact' ? 'outer-active' : undefined
-              }`}
-            >
-              <div
-                className={`inner-square ${
-                  page !== 'contact' ? 'inner-active' : undefined
-                }`}
-              ></div>
-            </div>
-            <p className={page !== 'contact' ? 'text-active' : undefined}>
-              Investment plans
-            </p>
-          </div>
-          <div
-            className={`rectangle ${
-              page !== 'contact' ? 'outer-active' : undefined
-            }`}
-          ></div>
-          <div
-            className={`rectangle ${
-              page !== 'contact' ? 'outer-active' : undefined
-            }`}
-          ></div>
-        </div>
-        <div className='progress'>
-          <div className='square-text-container'>
-            <div
-              className={`outer-square ${
-                page === 'preferences' ? 'outer-active' : undefined
-              }`}
-            >
-              <div
-                className={`inner-square ${
-                  page === 'preferences' ? 'inner-active' : undefined
-                }`}
-              ></div>
-            </div>
-            <p className={page === 'preferences' ? 'text-active' : undefined}>
-              Investment preferences
-            </p>
-          </div>
-        </div>
-      </div>
+      <Progress />
       <div className='quote-container'>
         <div className='quote-icon'></div>
         <p>{text}</p>
