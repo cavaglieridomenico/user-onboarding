00192cbe114bf2ce38e510cec78295cb3e535ff5
cavaglieridomenico <domenicomassimo.cavaglieri@gmail.com>
2021-11-16 06:05:16 +0100

    [ADDED] cursor 1 controls input 1
    Moving the slider 1 (from value) changes the value of input 1
    accordingly.

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,34 +1,60 @@
 import React, { useRef, useState, useEffect } from 'react';
 
 const Slider = ({ fromValue, toValue, handleFromValue, handleToValue }) => {
-  const [cursorOn, setCursorOn] = useState(false);
+  const [cursorOn1, setCursorOn1] = useState(false);
+  const [cursorOn2, setCursorOn2] = useState(false);
 
   const slider = useRef(null);
   const cursor1 = useRef(null);
   const cursor2 = useRef(null);
 
   useEffect(() => {
+    const sliderLeft = slider.current.getBoundingClientRect().left;
     cursor1.current.style.left = fromValue + 'px';
     cursor2.current.style.left = toValue + 'px';
-    const handleMouseMove = event => {
-      const sliderLeft = slider.current.getBoundingClientRect().left;
+    const handleMouseMove1 = event => {
+      const newLeft = event.clientX - 16 - sliderLeft;
+      handleFromValue(newLeft);
+      cursor1.current.style.left = newLeft + 'px';
+      console.log(newLeft);
+    };
+    const handleMouseMove2 = event => {
       const newLeft = event.clientX - 16 - sliderLeft;
       handleToValue(newLeft);
       cursor2.current.style.left = newLeft + 'px';
       console.log(newLeft);
     };
-    if (cursorOn) {
-      window.addEventListener('mousemove', handleMouseMove);
+    if (cursorOn1) {
+      window.addEventListener('mousemove', handleMouseMove1);
       window.addEventListener('mouseup', () => {
-        setCursorOn(false);
-        window.removeEventListener('mousemove', handleMouseMove);
+        setCursorOn1(false);
+        window.removeEventListener('mousemove', handleMouseMove1);
       });
     }
-  }, [cursorOn, fromValue, toValue, handleToValue]);
+    if (cursorOn2) {
+      window.addEventListener('mousemove', handleMouseMove2);
+      window.addEventListener('mouseup', () => {
+        setCursorOn2(false);
+        window.removeEventListener('mousemove', handleMouseMove2);
+      });
+    }
+  }, [
+    cursorOn1,
+    handleFromValue,
+    cursorOn2,
+    fromValue,
+    toValue,
+    handleToValue,
+  ]);
 
-  const handleMousedown = event => {
+  const handleMousedown1 = event => {
     event.preventDefault();
-    setCursorOn(true);
+    setCursorOn1(true);
+  };
+
+  const handleMousedown2 = event => {
+    event.preventDefault();
+    setCursorOn2(true);
   };
 
   return (
@@ -42,12 +68,17 @@ const Slider = ({ fromValue, toValue, handleFromValue, handleToValue }) => {
         <div className='notch'></div>
       </div>
       <div className='slider' ref={slider}>
-        <div className='cursor' id='cursor-1' ref={cursor1}></div>
+        <div
+          className='cursor'
+          id='cursor-1'
+          ref={cursor1}
+          onMouseDown={handleMousedown1}
+        ></div>
         <div
           className='cursor'
           id='cursor-2'
           ref={cursor2}
-          onMouseDown={handleMousedown}
+          onMouseDown={handleMousedown2}
         ></div>
       </div>
     </div>
