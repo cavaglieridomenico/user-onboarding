fffe54ad7b86912e025758bd65dd512577fa5bf8
cavaglieridomenico <domenicomassimo.cavaglieri@gmail.com>
2021-11-16 10:35:52 +0100

    [ADDED] Start and end limits of the slider

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,29 +1,50 @@
 import React, { useRef, useState, useEffect } from 'react';
 
 const Slider = ({ fromValue, toValue, handleFromValue, handleToValue }) => {
-  const [cursorOn1, setCursorOn1] = useState(false);
-  const [cursorOn2, setCursorOn2] = useState(false);
-
   const slider = useRef(null);
   const cursor1 = useRef(null);
   const cursor2 = useRef(null);
 
+  const [shiftX1, setShiftX1] = useState(0);
+  const [shiftX2, setShiftX2] = useState(0);
+
+  const [cursorOn1, setCursorOn1] = useState(false);
+  const [cursorOn2, setCursorOn2] = useState(false);
+
   useEffect(() => {
-    const sliderLeft = slider.current.getBoundingClientRect().left;
     cursor1.current.style.left = fromValue + 'px';
     cursor2.current.style.left = toValue + 'px';
+
+    const slideStart = slider.current.getBoundingClientRect().left;
+    const slideEnd = slider.current.offsetWidth - cursor1.current.offsetWidth;
+
     const handleMouseMove1 = event => {
-      const newLeft = event.clientX - 16 - sliderLeft;
+      let newLeft = event.clientX - shiftX1 - slideStart;
+      if (newLeft < 0) {
+        newLeft = 0;
+      }
+
+      if (newLeft > slideEnd) {
+        newLeft = slideEnd;
+      }
+
       handleFromValue(newLeft);
       cursor1.current.style.left = newLeft + 'px';
-      console.log(newLeft);
     };
+
     const handleMouseMove2 = event => {
-      const newLeft = event.clientX - 16 - sliderLeft;
+      let newLeft = event.clientX - shiftX2 - slideStart;
+      if (newLeft < 0) {
+        newLeft = 0;
+      }
+
+      if (newLeft > slideEnd) {
+        newLeft = slideEnd;
+      }
       handleToValue(newLeft);
       cursor2.current.style.left = newLeft + 'px';
-      console.log(newLeft);
     };
+
     if (cursorOn1) {
       window.addEventListener('mousemove', handleMouseMove1);
       window.addEventListener('mouseup', () => {
@@ -31,6 +52,7 @@ const Slider = ({ fromValue, toValue, handleFromValue, handleToValue }) => {
         window.removeEventListener('mousemove', handleMouseMove1);
       });
     }
+
     if (cursorOn2) {
       window.addEventListener('mousemove', handleMouseMove2);
       window.addEventListener('mouseup', () => {
@@ -39,21 +61,23 @@ const Slider = ({ fromValue, toValue, handleFromValue, handleToValue }) => {
       });
     }
   }, [
+    fromValue,
+    toValue,
     cursorOn1,
     handleFromValue,
     cursorOn2,
-    fromValue,
-    toValue,
     handleToValue,
   ]);
 
   const handleMousedown1 = event => {
     event.preventDefault();
+    setShiftX1(event.clientX - cursor1.current.getBoundingClientRect().left);
     setCursorOn1(true);
   };
 
   const handleMousedown2 = event => {
     event.preventDefault();
+    setShiftX2(event.clientX - cursor2.current.getBoundingClientRect().left);
     setCursorOn2(true);
   };
 
@@ -73,6 +97,7 @@ const Slider = ({ fromValue, toValue, handleFromValue, handleToValue }) => {
           id='cursor-1'
           ref={cursor1}
           onMouseDown={handleMousedown1}
+          onDragStart={() => false}
         ></div>
         <div
           className='cursor'
