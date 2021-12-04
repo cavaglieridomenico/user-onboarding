0b85b5eef75abe16d079b1073a8a18162fbddc2b
cavaglieridomenico <domenicomassimo.cavaglieri@gmail.com>
2021-12-04 17:59:32 +0100

    [ADDED] lists.js
    This file contains the lists with the data to be rendered in the
    project. It replaces the previous preferencesList.js

---
+++ src/assets/scripts/lists.js
@@ -0,0 +1,21 @@
+/*List of amount values on the Plans.js page*/
+export const plansList = [
+  { label: '$10,000', value: 10000 },
+  { label: '$50,000', value: 50000 },
+  { label: '$100,000', value: 100000 },
+  { label: '$200,000', value: 200000 },
+  { label: '$500,000', value: 500000 },
+  { label: '$1,000,000', value: 1000000 },
+];
+
+/*List of preferences on the Preferences.js page*/
+export const preferencesList = [
+  'Single family',
+  'Residential multifamily',
+  'Commercial retail',
+  'Commercial industrial',
+  'Commercial hospitality',
+  'Commercial warehousing',
+  'Commercial office',
+  'Other',
+];
